ba19bf84b5d624484009fd1b87a5fdadc798dbbd
kyin42 <jochkn@gmail.com>
2016-12-14 16:58:22 -0700

    changed port number for heroku

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.use(function (req, res, next) {
   res.status(404).send('404! Not found.')
 })
 
-app.listen(8080, function () {
-  console.log('Example app listening on port ' + 8080);
+app.listen(3000, function () {
+  console.log('Example app listening on port ' + 3000);
 });
