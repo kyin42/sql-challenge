f67ffd1679acd5d47786b904b0c1976695a97075
kyin42 <jochkn@gmail.com>
2016-12-14 20:41:43 -0700

    another failed test

--- app.js
+++ app.js
@@ -20,13 +20,14 @@ app.get('\/((index\.html)?)', function (req, res) {
 var pg = require('pg');
 
 app.get('/db', function (request, response) {
+	console.log(process.env.DATABASE_URL);
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM posts', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render('pages/db', {results: result.rows} ); }
+       { response.render('views/db', {results: result.rows} ); }
     });
   });
 });
