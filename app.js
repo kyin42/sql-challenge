248a13c36ef44c7ef61c7fdeefe213fe65860efd
kyin42 <jochkn@gmail.com>
2016-12-15 12:48:15 -0700

    missed a file

--- app.js
+++ app.js
@@ -15,33 +15,17 @@ app.set('views', __dirname+'/views');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
+var pg = require('pg');
+
 app.get('\/((index\.html)?)', function (req, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done){
 		client.query('SELECT * FROM posts',function(err, result){
 			done();
-			console.log(result.rows[0]);
 			if(err){
 				console.error(err);
-				response.send("Error from db " + err);
+				res.render("./index.ejs", {dbsuccess: false});
 			} else{
-  				res.render("./db.ejs", {results: result.rows});
-			}
-		});
-	});
-  res.render("./index.ejs");
-});
-var pg = require('pg');
-
-app.get('\/db', function (req, res) {
-	pg.connect(process.env.DATABASE_URL, function(err, client, done){
-		client.query('SELECT * FROM posts',function(err, result){
-			done();
-			console.log(result.rows[0]);
-			if(err){
-				console.error(err);
-				response.send("Error from db " + err);
-			} else{
-  				res.render("./db.ejs", {results: result.rows});
+  				res.render("./index.ejs", {dbsuccess: true,results: result.rows});
 			}
 		});
 	});
