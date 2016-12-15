f38d8d0d5ee26e6ca2744e171dd50e3c160a1f98
kyin42 <jochkn@gmail.com>
2016-12-14 18:46:45 -0700

    test

--- app.js
+++ app.js
@@ -17,6 +17,19 @@ app.get('\/((index\.html)?)', function (req, res) {
   res.render("./index.html");
 });
 
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 
 app.get('/css/:path', function (req, res) {
   res.sendFile(__dirname + '/css/' + req.params.path);
