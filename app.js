a1e476d692d0d491bfbc59bbd7d6df5fbef4f97e
kyin42 <jochkn@gmail.com>
2016-12-14 21:05:53 -0700

    ugh

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get('/db', function (request, response) {
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render('./db.html'/*, {results: result.rows} */); }
+       { response.render("/db.html"/*, {results: result.rows} */); }
     });
   });
 });
