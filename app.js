eaaab8982e7eca79e3337e7b9f512cacdaa61792
kyin42 <jochkn@gmail.com>
2016-12-14 17:22:58 -0700

    changed port settings, again :(

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ app.use(function (req, res, next) {
   res.status(404).send('404! Not found.')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port ' + 3000);
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port ' + app.get('port'));
 });
